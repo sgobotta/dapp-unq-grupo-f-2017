f1a150311fd1d8c742c49adf58dfd184ff1c2d0f
Santiago <sgobotta@gmail.com>
2017-08-24 21:10:47 -0300

    Frontend: Routing/Views Configuration
    + The new Router is configured with the routes file
    + Basic components to call the new route have been added to the main html

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,20 +5,28 @@ import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from "./app.routes";
+import { Maps } from "./views/maps/maps";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Maps
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppModule { }
