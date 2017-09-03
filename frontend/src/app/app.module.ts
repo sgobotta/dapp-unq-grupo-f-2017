e08468c0d4735ec10abfcb2580a75aebffac5f50
Santiago <sgobotta@gmail.com>
2017-09-03 03:04:25 -0300

    Frontend: Maps View
    + A radius component has been added to the map
    + A marker can be added to the map

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
 import { MaterialModule } from "@angular/material";
--- frontend/src/app/views/maps/maps.ts
+++ frontend/src/app/views/maps/maps.ts
@@ -9,6 +9,53 @@ import { Component } from "@angular/core";
 export class Maps {
 
   title = "Maps";
-  lat: number = -34.706556;
-  lng: number = -58.2807111;
+
+  // UNQ coordinates
+  latitude: number = -34.706556;
+  longitude: number = -58.2807111;
+
+  map = {
+    zoom: 13
+  }
+
+  radioTarget = {
+    latitude: this.latitude,
+    longitude: this.longitude,
+    radius: 2200,
+    fillColor: "green",
+    draggable: false,
+    editable: false,
+    clickable: false
+  }
+
+  markers: marker[] = []
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
+  onMapClicked($event) {
+    var coordinates = $event.coords;
+
+    // The list is empty so that only one marker can be added (can be modified)
+    this.markers = [];
+    this.markers.push({
+      lat: coordinates.lat,
+      lng: coordinates.lng,
+      label: 'Destination',
+      draggable: true
+    });
+  }
+
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+}
+
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable?: boolean;
 }
