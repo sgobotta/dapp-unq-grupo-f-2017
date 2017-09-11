0bdb37db11afc3a46aa65fc4e56695fe5d60e3ce
Santiago <sgobotta@gmail.com>
2017-09-10 22:48:48 -0300

    Backend: Menu model
    + Menu, Currency classes have been created

--- backend/src/models/menu.ts
+++ backend/src/models/menu.ts
@@ -1,8 +1,19 @@
-
+import Currency from "./utils/currency";
 
 export class Menu {
 
+  name: string;
+  description: string;
+  category: [string];
+  deliveryPrice: Currency;
+  validityRange: [Date];
+  deliveryTimeRange: [Date];
+  price: Currency;
+  minQuantity: number;
+  minQuantityPrice: Currency;
+  maxDailySalesQuantity: number;
+
   constructor() {
-    
+
   }
 }
