a715f331329df16a5abcb82af4ddff5e8dc542ff
Santiago <sgobotta@gmail.com>
2017-09-10 22:43:47 -0300

    Backend: Provider model
    + A Provider class has been created
    + Created classes a Provider depends on: Address, Phone, Availability, MapsPolygon, Reputation

--- backend/src/models/provider.ts
+++ backend/src/models/provider.ts
@@ -1,7 +1,23 @@
+import Address from "./utils/address";
+import Phone from "./utils/phone";
+import Availability from "./utils/availability";
+import MapsPolygon from "./utils/maps/maps-polygon";
+import Reputation from "./reputation";
 
 export class Provider {
 
+  name: string;
+  logo: string;
+  address: Address;
+  description: string;
+  website: string;
+  email: string;
+  phone: Phone;
+  availability: Availability;
+  deliveryLocationRange: MapsPolygon;
+  reputation: Reputation;
+
   constructor() {
-    
+
   }
 }
---
+++ backend/src/models/utils/maps/maps-polygon.ts
@@ -0,0 +1,8 @@
+
+
+export default class MapsPolygon {
+
+  constructor() {
+
+  }
+}
