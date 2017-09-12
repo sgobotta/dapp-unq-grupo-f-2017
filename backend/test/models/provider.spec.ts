528e4b992ba4df8ca648c15780fbae8d4bb9d54f
Santiago <sgobotta@gmail.com>
2017-09-11 23:09:10 -0300

    Backend: Test Provider
    + A new basic test has been added

--- backend/test/models/provider.spec.ts
+++ backend/test/models/provider.spec.ts
@@ -1,1 +1,10 @@
-// to-do
+import { Provider } from "./../../src/models/provider";
+import { expect } from "chai";
+import "mocha";
+
+describe("Provider class when it's created", () => {
+  it("should not be null", () => {
+    const provider = new Provider();
+    expect(provider).to.not.equal("undefined");
+  });
+});
