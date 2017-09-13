da308b622b224503b0858aa4da0fcaaf3d690766
Santiago <sgobotta@gmail.com>
2017-09-12 21:00:11 -0300

    Backend: Mailing System
    + A new method has been implemented to send a hardcoded mail

--- backend/src/models/system/mailing-system.ts
+++ backend/src/models/system/mailing-system.ts
@@ -1,13 +1,36 @@
 import * as schedule from "node-schedule";
+import * as nodemailer from "nodemailer";
+import * as mailgun from "mailgun-js";
 
 export class MailingSystem {
 
   constructor() {}
 
   public startJobs() {
-    schedule.scheduleJob("30 * * * * *", function() {
+    schedule.scheduleJob("30 * * * * *", () => {
+
+      this.sendEmail();
       console.log("This is a midnight log");
     });
+  }
+
+  sendEmail() {
+
+    var api_key = 'key-0bd4b5d729416d246e38a999f9e78a89';
+    var domain = 'morfi-ya.grupo-f.com';
+    var mg = mailgun({apiKey: api_key, domain: domain});
+
+    var data = {
+      from: 'Excited User <me@samples.mailgun.org>',
+      to: 'sgobotta@gmail.com, juanma.rdefago@gmail.com, discosdevictoria@gmail.com',
+      subject: 'Ghost Booh',
+      text: 'Testing some Mailgun awesomness!'
+    };
+
+    mg.messages().send(data, function (error, body) {
+      console.log(body);
+    });
+
   }
 
 }
