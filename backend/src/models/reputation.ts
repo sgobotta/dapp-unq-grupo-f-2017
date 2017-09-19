e036f32beccec804d227c2e127f36c17616ac10b
Santiago <sgobotta@gmail.com>
2017-09-19 13:07:05 -0300

    Backend: Reputation Refactor
    + Moved logic from rate to a new method that calculates the average rating

--- backend/src/models/reputation.ts
+++ backend/src/models/reputation.ts
@@ -11,12 +11,16 @@ export class Reputation {
   public rate(value) : void {
   	if(this.averageRating !== null){
   		this.quantity++;
-  		this.averageRating = this.averageRating * ((this.quantity - 1) / this.quantity) + (value / this.quantity);
+  		this.averageRating = this.calculateRating(value);
   	} else {
   		this.quantity = 1;
   		this.averageRating = value;
   	}
   }
+
+	private calculateRating(value): number {
+		return this.averageRating * ((this.quantity - 1) / this.quantity) + (value / this.quantity);
+	}
 
   public getRating() : number {
   	return this.averageRating;
