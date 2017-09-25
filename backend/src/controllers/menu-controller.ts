547d65c6568b68e85124e53e8734570b651429f3
Santiago <sgobotta@gmail.com>
2017-09-25 00:03:16 -0300

    Menu Controller types modification
    + Return types for the controller methods have been changed to Promise of Menu

--- backend/src/controllers/menu-controller.ts
+++ backend/src/controllers/menu-controller.ts
@@ -1,40 +1,40 @@
 import {
   controller, httpGet, httpPost, httpPut, httpDelete
-} from 'inversify-express-utils';
-import { injectable, inject } from 'inversify';
-import { MenuService } from './../services/menu-service';
-import { IMenu } from "./../services/interfaces";
-import { Request } from 'express';
-import TYPES from './../constants/types';
+} from "inversify-express-utils";
+import { injectable, inject } from "inversify";
+import { MenuService } from "./../services/menu-service";
+import Menu from "./../models/menu";
+import { Request } from "express";
+import TYPES from "./../constants/types";
 
 @injectable()
-@controller('/menu')
+@controller("/menu")
 export class MenuController {
 
   constructor(@inject(TYPES.MenuService) private menuService: MenuService) { }
 
-  @httpGet('/')
-  public getUsers(): IMenu[] {
-    return this.menuService.getUsers();
+  @httpGet("/")
+  public getMenus(): Promise<Menu[]> {
+    return this.menuService.getMenus();
   }
 
-  @httpGet('/:id')
-  public getUser(request: Request): IMenu {
+  @httpGet("/:id")
+  public getMenu(request: Request): Promise<Menu> {
     return this.menuService.getMenu(request.params.id);
   }
 
-  @httpPost('/')
-  public newUser(request: Request): IMenu {
+  @httpPost("/")
+  public newMenu(request: Request): Promise<Menu> {
     return this.menuService.newMenu(request.body);
   }
 
-  @httpPut('/:id')
-  public updateUser(request: Request): IMenu {
+  @httpPut("/:id")
+  public updateMenu(request: Request): Promise<Menu> {
     return this.menuService.updateMenu(request.params.id, request.body);
   }
 
-  @httpDelete('/:id')
-  public deleteUser(request: Request): string {
+  @httpDelete("/:id")
+  public deleteMenu(request: Request): Promise<Menu> {
     return this.menuService.deleteMenu(request.params.id);
   }
 }
