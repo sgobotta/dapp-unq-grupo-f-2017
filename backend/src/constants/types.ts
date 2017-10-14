7409c2c113465550b419bcc381883b64d4bccf2e
Santiago <sgobotta@gmail.com>
2017-10-14 15:35:39 -0300

    Backend: TYPES
    + Added a new TYPE for Mysql Database startup module, so it can be injected to the server startup

--- backend/src/constants/types.ts
+++ backend/src/constants/types.ts
@@ -1,7 +1,8 @@
 const TYPES = {
   MenuService: Symbol("MenuService"),
   MySQLClient: Symbol("MySQLClient"),
-  MongoDBClient: Symbol("MongoDBClient")
+  MongoDBClient: Symbol("MongoDBClient"),
+  MysqlDb: Symbol("MysqlDb")
 };
 
 export default TYPES;
