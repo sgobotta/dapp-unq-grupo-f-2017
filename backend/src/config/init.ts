8c42e1979c80d00d3e026c48416783275c5298b2
Santiago <sgobotta@gmail.com>
2017-10-16 21:30:39 -0300

    Backend: Database Initialization
    + MongoClient has been removed from the db init module
    + Implemented functionality for fake data import to MongoDB Menu Collection

--- backend/src/config/init.ts
+++ backend/src/config/init.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "inversify";
+import { Collection } from "mongodb";
 import { MySQLClient } from "./mysql/client";
-import { MongoDBClient } from "./mongodb/client";
+import { MongoDBConnection } from "./mongodb/connection";
 import TYPES from "./../constants/types";
 import * as path from "path";
 import * as fs from "fs";
@@ -9,17 +10,16 @@ import * as fs from "fs";
 export default class Startup {
 
   private mySqlClient: MySQLClient;
-  private mongoDBClient: MongoDBClient;
 
   constructor(
-    @inject(TYPES.MySQLClient) mySqlClient: MySQLClient,
-    @inject(TYPES.MongoDBClient) mongoDBClient: MongoDBClient
+    @inject(TYPES.MySQLClient) mySqlClient: MySQLClient
   ) {
     this.mySqlClient = mySqlClient;
-    this.mongoDBClient = mongoDBClient;
   }
 
-  public loadDatabase() {
+  public loadDatabases() {
+
+    this.loadMongoCollections();
 
     let tablesInfo = fs.readFileSync(path.join(__dirname + "./../../config/initial_data.sql"), "utf8");
 
@@ -27,6 +27,38 @@ export default class Startup {
     //   if(err) throw err;
     //   console.log(res);
     // });
+  }
+
+  private getConnection(callback) {
+    MongoDBConnection.getConnection((connection) => {
+      if(!connection) console.log("No databases found")
+      callback(connection);
+    })
+  }
+
+  private dropCollection(collection: Collection) {
+    collection.drop((err, res) => {
+      if(err) throw err;
+    });
+  }
+
+  private importCollection(collection: Collection, data) {
+    this.dropCollection(collection);
+    collection.insert(data, (error, insert) => {
+      if(error) throw error;
+    });
+
+  }
+
+  private loadMongoCollections() {
+
+    // TODO: implement get assets function from private data
+    let menuInfo = JSON.parse(fs.readFileSync(path.join(__dirname + "./../../private/data/", "menu.json"), "utf8"));
+
+    this.getConnection((connection) => {
+      this.importCollection(connection.collection("menu"), menuInfo);
+    });
+
   }
 
 }
