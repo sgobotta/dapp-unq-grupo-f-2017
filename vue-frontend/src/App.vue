074e55e2ab337d240970940ddc4fa41fc9e94eac
juanmardefago <juanma.rdefago@gmail.com>
2017-10-28 03:14:04 -0300

    Added background images that change, and other things

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -2,14 +2,51 @@
   <div id="app">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
     <link rel="stylesheet" href="http://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<md-whiteframe md-tag="md-toolbar" md-elevation="6" class="md-medium">
+
+      <div class="md-toolbar-container">
+        <md-button class="md-icon-button">
+          <md-icon>menu</md-icon>
+        </md-button>
+
+        <span style="flex: 1;">
+              <h2 class="md-title centered">{{ title }}</h2>
+        </span>
+
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button">
+          <md-icon>filter_list</md-icon>
+        </md-button>
+
+      </div>
+
+    </md-whiteframe>
+
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      title: 'MorfiYa!'
+    }
+  }
 }
 </script>
 
 <style src="vue-material/dist/vue-material.css"></style>
+
+<style> 
+
+.centered {
+  text-align: center;
+}
+
+</style>
---
+++ vue-frontend/src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+
+  <md-image :md-src="images[currentNumber % images.length]" class="image-background" :key="image">asd</md-image>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      images: ['/static/food-background1.jpg', '/static/food-background2.jpg', '/static/food-background3.jpg'],
+      currentNumber: 0,
+      timer: null
+    }
+  },
+
+  mounted: function () {
+    this.startRotation()
+  },
+
+  methods: {
+    startRotation: function () {
+      this.timer = setInterval(this.next, 6000)
+    },
+
+    stopRotation: function () {
+      clearTimeout(this.timer)
+      this.timer = null
+    },
+
+    next: function () {
+      this.currentNumber += 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.image-background {
+  background-color: black;
+}
+
+
+</style>
