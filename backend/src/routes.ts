8404c9a64cc8758bb75a1f40ef22dae96404dfac
Santiago <sgobotta@gmail.com>
2017-10-30 00:05:34 -0300

    Backend: Balance Service route
    + Added a balance controller and service to the routes setup

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,8 @@ import { ProviderController } from "./controllers/provider-controller";
 import { ProviderService } from "./services/provider-service";
 import { CustomerController } from "./controllers/customer-controller";
 import { CustomerService } from "./services/customer-service";
+import { BalanceController } from "./controllers/balance-controller";
+import { BalanceService } from "./services/balance-service";
 
 @injectable()
 export default class Routes {
@@ -27,6 +29,9 @@ export default class Routes {
 
     container.bind<interfaces.Controller>(TYPE.Controller).to(CustomerController).whenTargetNamed(TAGS.CustomerController);
     container.bind<CustomerService>(TYPES.CustomerService).to(CustomerService);
+
+    container.bind<interfaces.Controller>(TYPE.Controller).to(BalanceController).whenTargetNamed(TAGS.BalanceController);
+    container.bind<BalanceService>(TYPES.BalanceService).to(BalanceService);
   }
 
 }
