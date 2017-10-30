bc984970bb978b02b8a6d31957b449989f0f87a4
Santiago <sgobotta@gmail.com>
2017-10-30 00:04:55 -0300

    Backend: Balance Services
    + A new controller has been added for balance webservices
    + A new service has been created with a basic method

---
+++ backend/src/controllers/balance-controller.ts
@@ -0,0 +1,26 @@
+import {
+  controller, httpGet, httpPost, httpPut, httpDelete
+} from "inversify-express-utils";
+import { injectable, inject } from "inversify";
+import { BalanceService } from "../services/balance-service"
+import { Request, Response } from "express";
+import TYPES from "../constants/types";
+
+
+@injectable()
+@controller("/balance")
+export class BalanceController {
+
+  constructor(@inject(TYPES.BalanceService) private balanceService: BalanceService) { }
+
+  @httpGet("/:mail")
+  public getProviderBalance(request: Request, response: Response) {
+    return this.balanceService.getProviderBalanceByEmail(request.params.mail, response);
+  }
+
+  @httpGet("/customer/:mail")
+  public getCustomerBalanceByEmail(request: Request, response: Response) {
+    return this.balanceService.getCustomerBalanceByEmail(request.params.mail, response);
+  }
+
+}
---
+++ backend/src/services/balance-service.ts
@@ -0,0 +1,44 @@
+import { inject, injectable } from "inversify";
+import { MySQLClient } from "../config/mysql/client";
+import { Runner } from "../config/mysql/runner";
+import TYPES from "./../constants/types";
+import { ProviderBalanceBuilder } from "./../models/balance/provider-balance";
+
+@injectable()
+export class BalanceService {
+
+  private mySqlClient:MySQLClient;
+  private collection:string;
+
+  constructor(
+    @inject(TYPES.MySQLClient) mySqlClient:MySQLClient
+  ) {
+      this.mySqlClient = mySqlClient;
+      this.collection = "ProviderBalance";
+  }
+
+  public getProviderBalanceByEmail(providerId:string, response) {
+    return Runner.runInSession(() => {
+      let balance;
+      this.mySqlClient.findOneByProperty(this.collection, { providerId: providerId }, (err, res) => {
+        let provider = res[0];
+        if (err) throw err;
+        else if (provider !== undefined) {
+          balance = new ProviderBalanceBuilder()
+            .withId(provider.providerId)
+            .withAmount(provider.amount)
+            .withLastAccessDate(provider.lastAccessed)
+            .build();
+          response.send({ success: true, balance });
+          return;
+        }
+        response.send({ success: false })
+      });
+    });
+  }
+
+  public getCustomerBalanceByEmail(customerId:number, response) {
+
+  }
+
+}
