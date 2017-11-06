5751174280bc953defaf641109cacc98709db390
Santiago <sgobotta@gmail.com>
2017-11-05 22:28:15 -0300

    Backend: Logger configuration
    + Added configuration parameters to the logger

--- backend/src/logger/logger.ts
+++ backend/src/logger/logger.ts
@@ -5,20 +5,24 @@ export default class Logger {
   public static logger: winston.LoggerInstance;
 
   public static createLogger() {
+    const tsFormat = () => (new Date()).toLocaleTimeString();
     this.logger = new winston.Logger({
       transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: "morfi-ya.log" })
+        new winston.transports.Console({ colorize: true, align: true, handleExceptions: true }),
+        new winston.transports.File({ filename: "morfi-ya.log", json: true, colorize: true, handleExceptions: true })
       ]
     });
   }
 
   public static getLogger() {
+
     return this.logger;
   }
 
-  public static info(message) {
-    this.logger.info(message);
+
+
+  public static info(...params) {
+    this.logger.info(params.join());
   }
 
 
