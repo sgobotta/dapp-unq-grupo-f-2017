49cbd7fc79b0b5e3689aa88e8af08fc36e32b861
Santiago <sgobotta@gmail.com>
2017-11-20 18:59:40 -0300

    Backend: User Controller refactor
    + The controller now handles responses depending on the promise result

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -2,7 +2,7 @@ import {
   controller, httpGet, httpPost, httpPut, httpDelete
 } from "inversify-express-utils";
 import { injectable, inject } from "inversify";
-import { UserService } from "../services/user-service"
+import { UserService, UserResponse } from "../services/user-service"
 import { Request, Response } from "express";
 import TYPES from "../constants/types";
 import { User } from "../models/user"
@@ -15,34 +15,30 @@ export class UserController {
   constructor(@inject(TYPES.UserService) private userService: UserService) {}
 
   @httpGet("/:email")
-  public getUser(request: Request) : Promise<any> {
+  public getUser(request: Request) : Promise<UserResponse> {
     return this.userService.getUserByEmail(request.params.email)
-      .then((response) => {
-        return response;
-      })
-      .catch((response) => {
-        return response;
-      })
+      .then((response) => { return response; })
+      .catch((response) => { return response; });
   }
 
   @httpPut("/:email")
-  public updateUser(request: Request): Promise<User> {
-  	return this.userService.updateUserByEmail(request.params.email, request.body);
+  public updateUser(request: Request): Promise<UserResponse> {
+  	return this.userService.updateUserByEmail(request.params.email, request.body)
+      .then((response) => { return response; })
+      .catch((response) => { return response; });
   }
 
   @httpDelete("/:email")
-  public deleteUser(request: Request): Promise<User> {
-  	return this.userService.deleteUserByEmail(request.params.email);
+  public deleteUser(request: Request): Promise<UserResponse> {
+  	return this.userService.deleteUserByEmail(request.params.email)
+      .then((response) => { return response; })
+      .catch((response) => { return response; });
   }
 
   @httpPost("/")
-  public newUser(request: Request, response: Response): Promise<any> {
+  public newUser(request: Request, response: Response): Promise<UserResponse> {
   	return this.userService.newUser(request.body)
-      .then((response) => {
-        return(response);
-      })
-      .catch((response) => {
-        return(response);
-      });
+      .then((response) => { return(response); })
+      .catch((response) => { return(response); });
   }
 }
