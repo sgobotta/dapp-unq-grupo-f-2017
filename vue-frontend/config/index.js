21eb49803654f657a38ab3c26ff4d8dd58c2e6e1
Santiago <sgobotta@gmail.com>
2017-11-21 20:10:05 -0300

    Frontend: webpack build updates
    + Changed output directory for index and assets to the backend directory

--- vue-frontend/config/index.js
+++ vue-frontend/config/index.js
@@ -8,8 +8,8 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../backend/dist/client/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../backend/dist/client'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
