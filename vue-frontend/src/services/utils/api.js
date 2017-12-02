6d87e9292063292cfff08a8f5d26a59c0ebcbb2c
Santiago <sgobotta@gmail.com>
2017-12-02 00:15:05 -0300

    Frontend: fixes Codacy issues

--- vue-frontend/src/services/utils/api.js
+++ vue-frontend/src/services/utils/api.js
@@ -5,22 +5,22 @@ const apiUrl = 'http://localhost:3000'
 export default {
   get (url, request) {
     return Vue.http.get(apiUrl + url, request)
-    .then((response) => Promise.resolve(response.data))
-    .catch((error) => Promise.reject(error))
+      .then((response) => Promise.resolve(response.data))
+      .catch((error) => Promise.reject(error))
   },
   post (url, request) {
     return Vue.http.post(apiUrl + url, request)
-    .then((response) => Promise.resolve(response.data))
-    .catch((error) => Promise.reject(error))
+      .then((response) => Promise.resolve(response.data))
+      .catch((error) => Promise.reject(error))
   },
   put (url, request) {
     return Vue.http.put(apiUrl + url, request)
-    .then((response) => Promise.resolve(response.data))
-    .catch((error) => Promise.reject(error))
+      .then((response) => Promise.resolve(response.data))
+      .catch((error) => Promise.reject(error))
   },
   delete (url, request) {
     return Vue.http.delete(apiUrl + url, request)
-    .then((response) => Promise.resolve(response.data))
-    .catch((error) => Promise.reject(error))
+      .then((response) => Promise.resolve(response.data))
+      .catch((error) => Promise.reject(error))
   }
 }
