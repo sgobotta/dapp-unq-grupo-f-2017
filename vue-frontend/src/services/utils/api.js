e644f59fe4d861bd49f9d2a33bd86f4df07417ee
Santiago <sgobotta@gmail.com>
2017-12-05 14:58:40 -0300

    Frontend: env variables changes

--- vue-frontend/src/services/utils/api.js
+++ vue-frontend/src/services/utils/api.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 
-let apiUrl
-if (process.env.ON_DEPLOY) {
-  apiUrl = 'https://group-f-morfi-ya-node.herokuapp.com/' + process.env.PORT
-} else {
-  apiUrl = 'http://localhost:3000'
-}
+let apiUrl = process.env.API_URL + process.env.PORT
 
 export default {
   get (url, request) {
